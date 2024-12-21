7ce0ae88eccac4d9e4d8bcc42792e1dfafff0892
sanket <sanketmaheshwari96@gmail.com>
2024-12-21 09:48:55 +0530

    added basic prompt

--- template/prompttemplate.py
+++ template/prompttemplate.py
@@ -1,1 +1,15 @@
 from langchain import PromptTemplate
+
+
+template = """Question : {question}
+
+Answer : """
+
+prompt_template = PromptTemplate(
+                  template = template,
+                  input_variable = ['question']
+                  )
+
+# user question
+
+Question = "What is the capital of France?"
