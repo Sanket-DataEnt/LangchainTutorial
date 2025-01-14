c95df9c63c8ba037a5e1e9316736c3b1b35ad57e
sanket <sanketmaheshwari96@gmail.com>
2025-01-15 00:18:12 +0530

    added LLM Intro notebook in application

--- auth/config.py
+++ auth/config.py
@@ -1,7 +1,8 @@
 import os
 import json
+import sys
 
-with open("config.json") as f:
+with open("/Users/sanket/Personal/LLM/LangchainTutorial/config.json") as f:
     env_vars = json.load(f)
 
 HUGGINGFACEHUB_API_TOKEN = env_vars["HF_API_KEY"]
